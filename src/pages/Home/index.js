2b2acaf303dd175ed3d36a90b6c0263825ec8f16
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-08-12 22:15:37 -0300

    added fetch to test

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -53,3 +53,11 @@ export default function Home() {
     </Container>
   );
 }
+
+fetch('http://localhost:3000')
+  .then((response) => {
+    console.log('response', response);
+  })
+  .catch((error) => {
+    console.log('erro', error);
+  });
