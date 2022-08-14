4c26c4c3cf1e728c3fc02a99c9ee7f86b927d91d
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-08-14 01:33:51 -0300

    fixed organization of ListContacts (new is ListHeader) and added state to control Order

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import formatPhone from '../../utils/formatPhone';
 
 import {
-  Container, InputSearchContainer, Header, ListContainer, Card,
+  Container, InputSearchContainer, Header, ListHeader, Card,
 } from './styles';
 
 import arrow from '../../assets/images/icons/arrow.svg';
@@ -13,6 +13,7 @@ import trash from '../../assets/images/icons/trash.svg';
 
 export default function Home() {
   const [contacts, setContacts] = useState([]);
+  const [order, setOrder] = useState('asc');
 
   useEffect(() => {
     fetch('http://localhost:3001/contacts')
@@ -25,8 +26,10 @@ export default function Home() {
       });
   }, []);
 
-  function handleOrder() {
-    fetch('http://localhost:3001/contacts?orderBy=desc')
+  function handleToggleOrderBy() {
+    setOrder((prevState) => (prevState === 'asc' ? 'desc' : 'asc'));
+
+    fetch(`http://localhost:3001/contacts?orderBy=${order}`)
       .then(async (response) => {
         const json = await response.json();
         setContacts(json);
@@ -53,38 +56,36 @@ export default function Home() {
         <Link to="/new">Novo contato</Link>
       </Header>
 
-      <ListContainer>
-        <header>
-          <button type="button" onClick={handleOrder}>
-            <span>Nome</span>
-            <img src={arrow} alt="Arrow" />
-          </button>
-        </header>
+      <ListHeader>
+        <button type="button" onClick={handleToggleOrderBy}>
+          <span>Nome</span>
+          <img src={arrow} alt="Arrow" />
+        </button>
+      </ListHeader>
 
-        {contacts.map((contact) => (
-          <Card key={contact.id}>
-            <div className="info">
-              <div className="contact-name">
-                <strong>{contact.name}</strong>
-                {contact.category_name && (
-                  <small>{contact.category_name}</small>
-                )}
-              </div>
-              <span>{contact.email}</span>
-              <span>{formatPhone(contact.phone)}</span>
+      {contacts.map((contact) => (
+        <Card key={contact.id}>
+          <div className="info">
+            <div className="contact-name">
+              <strong>{contact.name}</strong>
+              {contact.category_name && (
+              <small>{contact.category_name}</small>
+              )}
             </div>
+            <span>{contact.email}</span>
+            <span>{formatPhone(contact.phone)}</span>
+          </div>
 
-            <div className="actions">
-              <Link to={`/edit/${contact.id}`}>
-                <img src={edit} alt="Edit" />
-              </Link>
-              <button type="button">
-                <img src={trash} alt="Delete" />
-              </button>
-            </div>
-          </Card>
-        ))}
-      </ListContainer>
+          <div className="actions">
+            <Link to={`/edit/${contact.id}`}>
+              <img src={edit} alt="Edit" />
+            </Link>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      ))}
 
     </Container>
   );
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -54,25 +54,21 @@ export const Header = styled.header`
   }
 `;
 
-export const ListContainer = styled.div`
+export const ListHeader = styled.header`
   margin-top: 24px;
+  margin-bottom: 8px;
 
-  header {
-    margin-bottom: 8px;
+  button {
+    background: transparent;
+    border: none;
+    display: flex;
+    align-items: center;
 
-    button {
-      background: transparent;
-      border: none;
-      display: flex;
-      align-items: center;
-
-      span {
-        margin-right: 8px;
-        font-weight: bold;
-        color: ${({ theme }) => theme.colors.primary.main}
-      }
+    span {
+      margin-right: 8px;
+      font-weight: bold;
+      color: ${({ theme }) => theme.colors.primary.main}
     }
-
   }
 `;
 
