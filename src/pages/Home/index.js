7af80ac72323ddb758a1661c559473df4ef94612
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-08-21 22:32:51 -0300

    frontend fix: useEffect infinite loop

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,6 @@
-import { useEffect, useState, useMemo } from 'react';
+import {
+  useEffect, useState, useMemo, useCallback,
+} from 'react';
 import { Link } from 'react-router-dom';
 
 import ContactsService from '../../services/ContactsService';
@@ -35,7 +37,7 @@ export default function Home() {
     ))
   ), [contacts, searchTerm]);
 
-  async function loadContacts() {
+  const loadContacts = useCallback(async () => {
     try {
       setIsLoading(true);
 
@@ -48,11 +50,11 @@ export default function Home() {
     } finally {
       setIsLoading(false);
     }
-  }
+  }, [order]);
 
   useEffect(() => {
     loadContacts();
-  }, [order]);
+  }, [loadContacts]);
 
   function handleToggleOrderBy() {
     setOrder((prevState) => (prevState === 'asc' ? 'desc' : 'asc'));
