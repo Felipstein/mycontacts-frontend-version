931c3f7600fa3b2fe79ddcc217d861dd800c7c21
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-08-23 00:52:45 -0300

    frontend feat: refactored HttpClient

--- src/services/ContactsService.js
+++ src/services/ContactsService.js
@@ -10,7 +10,7 @@ class ContactsService {
   }
 
   async createContact(contact) {
-    return this.httpClient.post('/contacts', contact);
+    return this.httpClient.post('/contacts', { body: contact });
   }
 }
 
--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -6,34 +6,39 @@ export default class HttpClient {
     this.baseUrl = baseUrl;
   }
 
-  async get(path) {
-    await delay(500);
-
-    const response = await fetch(`${this.baseUrl}${path}`);
-
-    let body = null;
-    const contentType = response.headers.get('Content-Type');
-    if (contentType.includes('application/json')) {
-      body = await response.json();
-    }
-
-    if (response.ok) {
-      return body;
-    }
-
-    throw new APIError(response, body);
+  get(path, options) {
+    return this.makeRequest(path, {
+      method: 'GET',
+      headers: options?.headers,
+    });
   }
 
-  async post(path, body) {
+  post(path, options) {
+    return this.makeRequest(path, {
+      method: 'POST',
+      body: options?.body,
+      headers: options?.headers,
+    });
+  }
+
+  async makeRequest(path, options) {
     await delay(500);
 
-    const headers = new Headers({
-      'Content-Type': 'application/json',
-    });
+    const headers = new Headers();
+
+    if (options.body) {
+      headers.append('Content-Type', 'application/json');
+    }
+
+    if (options.headers) {
+      Object.entries(options.headers).forEach(([key, value]) => {
+        headers.append(key, value);
+      });
+    }
 
     const response = await fetch(`${this.baseUrl}${path}`, {
-      method: 'POST',
-      body: JSON.stringify(body),
+      method: options.method,
+      body: JSON.stringify(options.body),
       headers,
     });
 
