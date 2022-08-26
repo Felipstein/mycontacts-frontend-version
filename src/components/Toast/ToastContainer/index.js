71441c2a73df04bb12e17dfe85f17c165145122c
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-08-25 22:20:34 -0300

    frontend feat: toast part final

--- src/components/Toast/ToastContainer/index.js
+++ src/components/Toast/ToastContainer/index.js
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import { Container } from './styles';
 
 import ToastMessage from '../ToastMessage';
 
 export default function ToastContainer() {
+  const [messages] = useState([
+    { id: Math.random(), type: 'default', text: 'Default toast' },
+    { id: Math.random(), type: 'danger', text: 'Error toast' },
+    { id: Math.random(), type: 'success', text: 'Success toast' },
+  ]);
+
   return (
     <Container>
-      <ToastMessage text="Default toast" />
-      <ToastMessage text="Error toast" type="danger" />
-      <ToastMessage text="Success toast" type="success" />
+      {messages.map((message) => (
+        <ToastMessage
+          key={message.id}
+          type={message.type}
+          text={message.text}
+        />
+      ))}
     </Container>
   );
 }
