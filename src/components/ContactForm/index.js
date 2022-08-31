773fd5a1523cc812ee9b854732be21cfed492f1f
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-08-30 21:20:50 -0300

    frontend feat: cleaning ContactForm after submit

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -86,6 +86,10 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
     });
 
     setIsSubmitting(false);
+    setName('');
+    setEmail('');
+    setPhone('');
+    setCategoryId('');
   }
 
   return (
